52bf05d7bffc0d648272508b0355a7b3a4601ab9
Ross Hepburn <hepburnjrl@gmail.com>
2014-04-15 17:38:43 +0100

    Photo can be uploaded and added to S3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+InstagramClone::Application.config.secret_key_base = '529ee8b359765a6ff9ccc6562b2df5711da34cb96a27cd47e4a1d174327ce4a4dfabdc8a5055e9d5138936ba7f83f756742bb2105b2b46b1ca7a3b3e121851fd'
